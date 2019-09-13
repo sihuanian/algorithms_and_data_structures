c016ba5a54d879774b40c866a14cec7bc21f8eda
sihuanian <2267322300@qq.com>
2019-09-13 23:27:19 +0800

    冒泡排序的实现

---
+++ sored/bubbleSort.js
@@ -0,0 +1,28 @@
+/**
+ * @description: 冒泡排序
+ * @param Array
+ */
+function bubbleSort (array) {
+  const len = array.length
+  if (!Array.isArray(array) || len < 2) {
+    return
+  }
+  for (let i = 0; i < len - 1; i++) {
+    let flag = false
+    for (let j = 0; j < len - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+        flag = true 
+      }
+    }
+    if (!flag) {
+      break
+    }
+  }
+  return array
+}
+// const test = [4, 5, 6, 3, 2, 1]
+const test = [2]
+console.log(bubbleSort(test))
