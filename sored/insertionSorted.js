17a8d3cdcb2f5605132513b97fe74ff4c7ed674e
sihuanian <2267322300@qq.com>
2019-12-04 23:29:27 +0800

    重写一次

--- sored/insertionSorted.js
+++ sored/insertionSorted.js
@@ -2,7 +2,7 @@
  * @Author: sihuanian
  * @message: 插入排序（升序）
  */
-function insertionSorted(arr) {
+/* function insertionSorted(arr) {
   const len = arr.length
   // 未排序区间，默认是数组的第一项到最后一项
   for (let i = 1; i < arr.length; i++) {
@@ -24,4 +24,28 @@ function insertionSorted(arr) {
 
 const array = [4, 5, 6, 1, 3, 2]
 let sortedArr = insertionSorted(array)
+console.log(sortedArr) */
+
+
+function insert (arr) {
+  const length = arr.length
+  // 未排序区间
+  for (let i = 1; i < length; i++ ) {
+    const value = arr[i] // 未排序区间的第一项
+    // 有序区间 默认数组第一项是有序区间
+    let j = i - 1
+    for (; j >= 0; j--) {
+      if (value < arr[j]) {
+        arr[j + 1] = arr[j]
+      } else {
+        break
+      }
+    }
+    arr[j + 1] = value
+  }
+  return arr
+}
+
+const array = [4, 5, 6, 1, 3, 2]
+let sortedArr = insert(array)
 console.log(sortedArr)
